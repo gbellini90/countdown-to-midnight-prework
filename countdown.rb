b8b22028788bc6a6bd5acfdecf21f486f4e67426
Gabrielle Bellini <gbellini90@gmail.com>
2018-10-01 20:43:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,11 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(counter)
+ while counter>0
+  puts "#{counter} SECOND(S)!"
+  counter -= 1
+end
+"HAPPY NEW YEAR!"
+end
   
